412c5073a578ffd188687d1c24c8f0b4165a7d55
Gary Owl <garry19993@hotmail.com>
2026-06-14 16:08:38 +0800

    Use direct launch for Allbet and Playtech

--- laravel-api/resources/views/index.blade.php
+++ laravel-api/resources/views/index.blade.php
@@ -9,8 +9,8 @@
     />
     <link rel="icon" type="image/png" href="/logo.png" />
     <title>Elive</title>
-    <script type="module" crossorigin src="/assets/index-CqjA9LCM.js"></script>
-    <link rel="stylesheet" crossorigin href="/assets/index-BQ-AlAWO.css">
+    <script type="module" crossorigin src="/assets/index-DY2G-D_9.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/index-QNwPbSom.css">
   </head>
   <body>
     <div id="app"></div>
